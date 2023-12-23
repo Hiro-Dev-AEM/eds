b99df50ffa2e83bf9109f54cb06aa9bdc3f0299d
himai <himai@adobe.com>
2023-12-23 22:00:04 +0900

    fix

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,12 +10,15 @@ export default async function decorate(block) {// フッターの装飾を行う
   block.textContent = '';// block要素のテキストを削除
 
   // load footer fragment
-  const footerPath = footerMeta.footer || '/footer';// footerのパスが存在する場合は、footerのパスを取得。そうでない場合は、/footerを取得
-  const fragment = await loadFragment(footerPath);// footerのパスを引数にして、loadFragment関数を実行。戻り値をfragmentに代入
+  // footerのパスが存在する場合は、footerのパスを取得。そうでない場合は、/footerを取得
+  const footerPath = footerMeta.footer || '/footer';
+  // footerのパスを引数にして、loadFragment関数を実行。戻り値をfragmentに代入
+  const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
   const footer = document.createElement('div');// div要素を作成
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);// fragment要素の最初の子要素が存在する場合は、footer要素に追加
+  // fragment要素の最初の子要素が存在する場合は、footer要素に追加
+  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);// block要素の末尾にfooter要素を追加
 }
